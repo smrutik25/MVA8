9cc9ee6c4a06dc890f6ea22bab36afdf7fce852d
9KASTU <40312726+9KASTU@users.noreply.github.com>
2020-01-11 03:36:42 +0530

    DVA Game

---
+++ Game_App/game/app/src/main/java/com/example/game/Shapes.java
@@ -0,0 +1,20 @@
+package com.example.game;
+
+public class Shapes {
+
+    String name;
+    int image;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public Shapes(String name, int image){
+        this.name=name;
+        this.image=image;
+    }
+}
